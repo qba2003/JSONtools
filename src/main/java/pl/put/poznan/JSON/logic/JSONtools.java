ad07686c064fa643c8025679f68128b1ef594e1d
Mikhail Rybalko <147132802+MrBurzhuadotexe@users.noreply.github.com>
2024-12-17 02:20:07 +0100

    Update JSONtools.java

--- src/main/java/pl/put/poznan/JSON/logic/JSONtools.java
+++ src/main/java/pl/put/poznan/JSON/logic/JSONtools.java
@@ -15,30 +15,4 @@ public class JSONtools {
         // of course, normally it would do something based on the transforms
         return text.toUpperCase();
     }
-
-    public String prettyPrintJSON(String minifiedJson) {
-        try {
-            JSONObject json = new JSONObject(minifiedJson);
-            return json.toString(4); // 4 is the number of spaces for indentation
-        } catch (Exception e) {
-            return "Invalid JSON format: " + e.getMessage();
-        }
-    }
-
-    public String filterJSON(String json, String[] propertyNames) {
-        try {
-            JSONObject originalJson = new JSONObject(json);
-            JSONObject simplifiedJson = new JSONObject();
-
-            for (String property : propertyNames) {
-                if (originalJson.has(property)) {
-                    simplifiedJson.put(property, originalJson.get(property));
-                }
-            }
-
-            return simplifiedJson.toString(4); // 4 for pretty-printing
-        } catch (Exception e) {
-            return "Invalid JSON format: " + e.getMessage();
-        }
-    }
 }
