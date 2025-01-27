a94f4aaf9d7bd9fc74674988938b19f152def8f4
jakub <jakub.pilarski.2@student.put.poznan.pl>
2025-01-27 17:40:23 +0100

    uptd

--- src/main/java/pl/put/poznan/JSON/logic/JsonTools.java
+++ src/main/java/pl/put/poznan/JSON/logic/JsonTools.java
@@ -1,8 +1,5 @@
 package pl.put.poznan.JSON.logic;
 
-/**
- * This is just an example to show that the logic should be outside the REST service.
- */
 public class JsonTools {
 
     private final String[] transforms;
